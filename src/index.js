449957ad8e0e4725f3f2223db92c2da864630445
HrytsenkoEN <ivgenius@gmail.com>
2023-05-02 00:14:50 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -8,10 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
-
-
-const jsx1 = <span>Hello</span>
-const jsx2 = <span>World</span>
-const jsxElement = <div>{jsx1}{ jsx2}</div>
-console.log(jsxElement);

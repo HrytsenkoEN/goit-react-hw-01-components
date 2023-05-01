504400af95a2ba58aede1ae3fe6129e1738c18cc
HrytsenkoEN <ivgenius@gmail.com>
2023-05-02 00:01:35 +0300

    HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+
+import { Statistics } from '../components/Statistics/Statistics';
+import data from '../data/data.json';
+
+import { FriendList } from '../components/FriendList/FriendList';
+import friends from '../data/friends.json';
+
+import { TransactionHistory } from '../components/Transaction/TransactionHistory';
+import transactions from '../data/transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
+        padding: 50,
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        // justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        rowGap: 50,
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
+
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { Friends } from './Friends';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendslist}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <Friends
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
